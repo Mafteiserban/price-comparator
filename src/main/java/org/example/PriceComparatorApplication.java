ecd33a65520ccdc339b8ea9a81592245dc590281
Serban Maftei <serbanmaftei@gmail.com>
2025-05-22 23:46:42 +0300

    created product repository

--- src/main/java/org/example/PriceComparatorApplication.java
+++ src/main/java/org/example/PriceComparatorApplication.java
@@ -2,9 +2,11 @@ package org.example;
 
 import org.example.model.Discount;
 import org.example.model.Product;
+import org.example.repository.ProductRepository;
 import org.example.util.CsvReaderUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -12,19 +14,27 @@ public class PriceComparatorApplication {
     public static void main(String[] args) {
 
 
-       List<Product> products=CsvReaderUtil.readProducts("kaufland_2025-05-08.csv");
+//       List<Product> products=CsvReaderUtil.readProducts("kaufland_2025-05-08.csv");
+//
+//       for(Product product:products){
+//           System.out.println(product);
+//       }
+//
+//        System.out.println("---------------------------------------------------------------------------");
+//
+//       List<Discount> discounts = CsvReaderUtil.readDiscounts(" kaufland_discounts_2025-05-08.csv");
+//
+//       for(Discount discount:discounts){
+//           System.out.println(discount);
+//       }
+        ProductRepository repo = new ProductRepository();
 
-       for(Product product:products){
-           System.out.println(product);
-       }
+        List<Product> products = repo.getProductsByStoreAndDate("lidl", LocalDate.of(2025, 5, 1));
 
-        System.out.println("---------------------------------------------------------------------------");
-
-       List<Discount> discounts = CsvReaderUtil.readDiscounts(" kaufland_discounts_2025-05-08.csv");
-
-       for(Discount discount:discounts){
-           System.out.println(discount);
-       }
+        System.out.println("Produse Lidl - 2025-05-01:");
+        for (Product product : products) {
+            System.out.println(product);
+        }
 
     }
 
--- src/main/java/org/example/repository/ProductRepository.java
+++ src/main/java/org/example/repository/ProductRepository.java
@@ -1,4 +1,34 @@
 package org.example.repository;
 
+import org.example.model.Product;
+import org.example.util.CsvReaderUtil;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Repository
 public class ProductRepository {
+
+    private final Map<String, Map<LocalDate, List<Product>>> cache = new HashMap<>();
+
+    public List<Product> getProductsByStoreAndDate(String store, LocalDate date) {
+        cache.putIfAbsent(store, new HashMap<>());
+        Map<LocalDate, List<Product>> storeData = cache.get(store);
+
+        if (!storeData.containsKey(date)) {
+            String filename = store.toLowerCase() + "_" + date + ".csv";
+            List<Product> products = CsvReaderUtil.readProducts(filename);
+            storeData.put(date, products);
+        }
+
+        return storeData.get(date);
+    }
+
+
 }
+
+
